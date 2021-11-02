c22d3949a326d416f49f28e8066c089347e634f0
drrr <ray@pmalc.com>
2021-11-02 13:20:05 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,50 +4,25 @@ set -ex
 
 # Use the value of the corresponding environment variable, or the
 # default if none exists.
-: ${VAULTWARDEN_ROOT:="$(realpath "${0%/*}"/..)"}
-: ${SQLITE3:="/usr/bin/sqlite3"}
+: ${TARGET_ROOT:="$(realpath "${0%/*}"/..)"}
 : ${RCLONE:="/usr/local/bin/rclone"}
 : ${GPG:="/usr/bin/gpg"}
 : ${AGE:="/usr/local/bin/age"}
 
+# Change DATA_DIR
 DATA_DIR="data"
-BACKUP_ROOT="${VAULTWARDEN_ROOT}/backup"
-BACKUP_DIR_NAME="vaultwarden-$(date '+%Y%m%d-%H%M')"
+BACKUP_ROOT="${TARGET_ROOT}/backup"
+BACKUP_DIR_NAME="backup-$(date '+%Y%m%d-%H%M')"
 BACKUP_DIR_PATH="${BACKUP_ROOT}/${BACKUP_DIR_NAME}"
 BACKUP_FILE_DIR="archives"
 BACKUP_FILE_NAME="${BACKUP_DIR_NAME}.tar.xz"
 BACKUP_FILE_PATH="${BACKUP_ROOT}/${BACKUP_FILE_DIR}/${BACKUP_FILE_NAME}"
-DB_FILE="db.sqlite3"
 
 source "${BACKUP_ROOT}"/backup.conf
 
-cd "${VAULTWARDEN_ROOT}"
+cd "${TARGET_ROOT}"
 mkdir -p "${BACKUP_DIR_PATH}"
 
-# Back up the database using the Online Backup API (https://www.sqlite.org/backup.html)
-# as implemented in the SQLite CLI. However, if a call to sqlite3_backup_step() returns
-# one of the transient errors SQLITE_BUSY or SQLITE_LOCKED, the CLI doesn't retry the
-# backup step; instead, it simply stops the backup and returns an error. This is unlikely,
-# but to minimize the possibility of a failed backup, implement a retry mechanism here.
-max_tries=10
-tries=0
-until ${SQLITE3} "file:${DATA_DIR}/${DB_FILE}?mode=ro" ".backup '${BACKUP_DIR_PATH}/${DB_FILE}'"; do
-    if (( ++tries >= max_tries )); then
-	echo "Aborting after ${max_tries} failed backup attempts..."
-	exit 1
-    fi
-    echo "Backup failed. Retry #${tries}..."
-    rm -f "${BACKUP_DIR_PATH}/${DB_FILE}"
-    sleep 1
-done
-
-backup_files=()
-for f in attachments config.json rsa_key.der rsa_key.pem rsa_key.pub.der rsa_key.pub.pem sends; do
-    if [[ -e "${DATA_DIR}"/$f ]]; then
-        backup_files+=("${DATA_DIR}"/$f)
-    fi
-done
-cp -a "${backup_files[@]}" "${BACKUP_DIR_PATH}"
 tar -cJf "${BACKUP_FILE_PATH}" -C "${BACKUP_ROOT}" "${BACKUP_DIR_NAME}"
 rm -rf "${BACKUP_DIR_PATH}"
 md5sum "${BACKUP_FILE_PATH}"
